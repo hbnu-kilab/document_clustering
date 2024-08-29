a9ed1e3b12f5cc4eca2e5b72d5e79c70750217e9
lbyeh12 <lbye01122@gmail.com>
2024-08-29 17:35:33 +0900

    fix: BAAI/bge-multilingual-gemma2 임베딩 모델 변경 #2

--- clustering_document.py
+++ clustering_document.py
@@ -2,81 +2,25 @@ import os
 import threading
 from tqdm import tqdm  # 추가
 from dotenv import load_dotenv
-
+from accelerate import Accelerator
 load_dotenv() 
 api_token = os.getenv("api_token")
 os.environ["OPENAI_API_KEY"] = str(os.getenv("OPENAI_API_KEY"))
 
 from raptor import RetrievalAugmentation, RetrievalAugmentationConfig
 import jsonlines
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 import torch
 from raptor import BaseEmbeddingModel
 
+from sentence_transformers import SentenceTransformer
+
 class CustomEmbeddingModel(BaseEmbeddingModel):
-    def __init__(self, model_name='google/gemma-2-9b-it'):
-        try:
-            print("Starting model load...")
-            self.tokenizer = AutoTokenizer.from_pretrained(model_name, token=api_token)
-            print("Tokenizer loaded successfully")
-            
-            self.model = AutoModelForCausalLM.from_pretrained(model_name, token=api_token)
-            print("Model loaded successfully")
-        
-        except Exception as e:
-            print(f"Error occurred: {e}")
+    def __init__(self, model_name="BAAI/bge-multilingual-gemma2"):
+        self.model = SentenceTransformer(model_name, model_kwargs={"torch_dtype": torch.float16})
 
-    # def create_embedding(self, documents):
-    #     # GPU가 사용 가능한 경우 GPU로 모델을 이동
-    #     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    #     self.model.to(device)
-
-    #     embeddings = []
-    #     for doc in documents:
-    #     # 입력 데이터를 GPU로 이동
-    #         inputs = self.tokenizer(doc, return_tensors='pt', truncation=True, padding=True, max_length=512)
-    #         inputs = {key: value.to(device) for key, value in inputs.items()}
-
-    #         with torch.no_grad():
-    #             outputs = self.model(**inputs)
-            
-    #         # 출력을 CPU로 이동하고 numpy로 변환
-    #         # GPT 모델인 경우 logits 사용
-    #         if hasattr(outputs, 'logits'):
-    #             embedding = outputs.logits.mean(dim=1).squeeze().cpu().numpy()
-    #         # 다른 모델인 경우 last_hidden_state 사용
-    #         elif hasattr(outputs, 'last_hidden_state'):
-    #             embedding = outputs.last_hidden_state.mean(dim=1).squeeze().cpu().numpy()
-    #         else:
-    #             raise AttributeError("The model output does not have 'logits' or 'last_hidden_state' attributes.")
-    #         embeddings.append(embedding)
-    
-    #     return embeddings
-
-    def create_embedding(self, documents):
-        # CPU에서 모델을 사용하도록 설정
-        device = torch.device('cpu')
-        self.model.to(device)  # 모델을 CPU로 이동
-
-        embeddings = []
-        for doc in documents:
-            # 입력 데이터를 CPU로 이동 (이미 CPU에 있으므로 필요하지 않음)
-            inputs = self.tokenizer(doc, return_tensors='pt', truncation=True, padding=True, max_length=512)
-            # 입력 데이터를 명시적으로 CPU로 이동
-            inputs = {key: value.to(device) for key, value in inputs.items()}
-
-            with torch.no_grad():
-                 outputs = self.model(**inputs)
-
-             # 출력을 CPU에서 처리하고 numpy로 변환
-            if hasattr(outputs, 'logits'):
-                embedding = outputs.logits.mean(dim=1).squeeze().numpy()
-            elif hasattr(outputs, 'last_hidden_state'):
-                embedding = outputs.last_hidden_state.mean(dim=1).squeeze().numpy()
-            else:
-                raise AttributeError("The model output does not have 'logits' or 'last_hidden_state' attributes.")
-            embeddings.append(embedding)
-        return embeddings
+    def create_embedding(self, text):
+        return self.model.encode(text)   
         
 print("Starting model initialization...")
 custom_embedding = CustomEmbeddingModel()
@@ -99,7 +43,7 @@ documents = read_documents_from_jsonl(jsonl_file_path)
 
 RA.add_documents(documents) 
 
-SAVE_PATH = "C:/cluster/raptor/demo"
+SAVE_PATH = "C:/Users/USER/Desktop/clustering_doc/demo/doc"
 RA.save(SAVE_PATH)
 # RA = RetrievalAugmentation(tree=SAVE_PATH)
 
